5fff6446bb72c70a1588cede556de57ab2b2f77d
Kyle Derrick and Oskar Radon <oskarradonkimball@gmail.com>
2016-03-30 09:29:39 -0700

    add new city component

--- app/routes/index.js
+++ app/routes/index.js
@@ -14,6 +14,12 @@ export default Ember.Route.extend({
       this.transitionTo('index');
     },
 
+    saveCity(params) {
+      var newCity = this.store.createRecord('city', params);
+      newCity.save();
+      this.transitionTo('index');
+    },
+
     update(rental, params) {
       Object.keys(params).forEach(function(key) {
         if(params[key]!==undefined) {
