dd368af61d9a5a8f4e2a8b00527c8b9a40f0091c
Kyle Derrick and Oskar Radon <kderrick1988@gmail.com>
2016-03-30 09:01:12 -0700

    add city model

---
+++ app/routes/index.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      cities: this.store.findAll('city'),
+      rentals: this.store.findAll('rental')
+    });
+  },
+  actions: {
+    save3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
+
+    update(rental, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          rental.set(key,params[key]);
+        }
+      });
+      rental.save();
+      this.transitionTo('index');
+    },
+
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
+
+});
